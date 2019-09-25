17cc89820f28e0a23595055dccbaee7f3e1f34be
Aurélien PETIT <arpheus@free.fr>
2019-09-25 13:51:11 +0200

    Add tests for product create

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,6 @@
 # tests/test_views.py
 from flask_testing import TestCase
+from flask import request
 from wsgi import app
 
 class TestViews(TestCase):
@@ -22,3 +23,19 @@ class TestViews(TestCase):
         id = 5
         response = self.client.get(f"/api/v1/products/{id}")
         self.assertEqual(response.status,"404 NOT FOUND")
+
+    def test_create_products(self):
+        response = self.client.post(path='/api/v1/products',json='{"name": "toto"}')
+        json = response.json
+        #test the return code, should be 201 CREATED
+        self.assertEqual(response.status,201)
+        #test the returned object, should be a product with id and name
+        self.assertIs(["id","name"],list(json))
+
+    def test_create_fails_if_no_name_provided(self):
+        response = self.client.post(path='/api/v1/products',json='{}')
+        json = response.json
+        #test the return code, should be 422 UNPROCESSABLE ENTITY
+        self.assertEqual(response.status,422)
+        #test the returned object, should be None
+        self.assertEqual(json, None)
--- wsgi.py
+++ wsgi.py
@@ -42,5 +42,3 @@ def getProduct(post_id):
         PRODUCT = {}
         abort (404)
 
-
-
