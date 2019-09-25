00d836d906fd07396f798d3a7980ba0525d1d55a
Aurélien PETIT <arpheus@free.fr>
2019-09-25 11:05:43 +0200

    Add GET products api endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,17 @@
 # wsgi.py
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/api/v1/products')
+def getProducts():
+    PRODUCTS = [
+        { 'id': 1, 'name': 'Skello' },
+        { 'id': 2, 'name': 'Socialive.tv' }
+    ]
+    return jsonify(PRODUCTS)
